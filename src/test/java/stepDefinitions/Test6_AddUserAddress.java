d43250352eb0626449b7064c59a9a55b477f9e67
Sulaimon <topdandydot@gmail.com>
2023-07-29 16:28:00 +0300

    Cucumber with kubernetes test execution commit

---
+++ src/test/java/stepDefinitions/Test6_AddUserAddress.java
@@ -0,0 +1,36 @@
+package stepDefinitions;
+
+import base.TestBase;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.support.PageFactory;
+import pages.Test6_AddUserAddressPage;
+
+
+public class Test6_AddUserAddress extends TestBase {
+
+    @When("User clicks on email text link")
+    public void user_clicks_on_email_text_link() {
+        Test6_AddUserAddressPage page = PageFactory.initElements(driver, Test6_AddUserAddressPage.class);
+        page.userClicksOnEmailTextLink();
+    }
+
+    @When("User clicks on the addresses element")
+    public void user_clicks_on_the_addresses_element() {
+        Test6_AddUserAddressPage page = PageFactory.initElements(driver, Test6_AddUserAddressPage.class);
+        page.userClicksOnTheAddressElement();
+    }
+
+    @When("User adjust the necessary fields")
+    public void user_adjust_the_necessary_fields() {
+        Test6_AddUserAddressPage page = PageFactory.initElements(driver, Test6_AddUserAddressPage.class);
+        page.userAdjustTheNecessaryField();
+    }
+
+    @Then("User clicks on save button and validate successful update")
+    public void user_clicks_on_save_button_and_validate_successful_update() {
+        Test6_AddUserAddressPage page = PageFactory.initElements(driver, Test6_AddUserAddressPage.class);
+        page.userClicksOnSaveButtonAndValidateSuccessfulUpdate();
+    }
+
+}
